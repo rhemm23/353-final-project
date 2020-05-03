5db3d0a2f9520ba8c4753875a9654d262d563f58
Wyatt Dvorak <wyatt.f.dvorak@gmail.com>
2020-05-03 00:53:45 -0500

    Implemented io_expander_init

--- Project/io_expander.c
+++ Project/io_expander.c
@@ -2,7 +2,59 @@
 #include <io_expander.h>
 
 bool io_expander_init(void) {
-	
+
+  if(gpio_enable_port(IO_EXPANDER_GPIO_BASE) == false)
+  {
+    return false;
+  }
+  
+  // Configure SCL 
+  if(gpio_config_digital_enable(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SCL_PIN)== false)
+  {
+    return false;
+  }
+    
+  if(gpio_config_alternate_function(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SCL_PIN)== false)
+  {
+    return false;
+  }
+    
+  if(gpio_config_port_control(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SCL_PCTL_M, IO_EXPANDER_I2C_SCL_PIN_PCTL)== false)
+  {
+    return false;
+  }
+    
+
+  
+  // Configure SDA 
+  if(gpio_config_digital_enable(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SDA_PIN)== false)
+  {
+    return false;
+  }
+    
+  if(gpio_config_open_drain(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SDA_PIN)== false)
+  {
+    return false;
+  }
+    
+  if(gpio_config_alternate_function(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SDA_PIN)== false)
+  {
+    return false;
+  }
+    
+  if(gpio_config_port_control(IO_EXPANDER_GPIO_BASE, IO_EXPANDER_I2C_SDA_PCTL_M, IO_EXPANDER_I2C_SDA_PIN_PCTL)== false)
+  {
+    return false;
+  }
+    
+  //  Initialize the I2C peripheral
+  if( initializeI2CMaster(IO_EXPANDER_I2C_BASE)!= I2C_OK)
+  {
+    return false;
+  }
+  
+  return true;
+  
 }
 
 
